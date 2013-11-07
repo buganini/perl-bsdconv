4d6df6ef6247990ef33b4ccf33e6786a6a15bfb1
Buganini Q <buganini@gmail.com>
2013-11-07 19:41:53 +0800

    cleanup example

--- eg/example_codecs.pl
+++ eg/example_codecs.pl
@@ -1,10 +1,5 @@
 use bsdconv;
 
-$c=new bsdconv('utf-8:utf-8');
-print $c->toString();
-print "\n";
-print $c->conv('test');
-print "\n";
 print bsdconv::codec_check(bsdconv::FROM, '_utf-8');
 print "\n";
 print bsdconv::codec_check(bsdconv::INTER, '_utf-8');
